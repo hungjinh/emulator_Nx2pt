8bd28ea598a0e9e379dd34563fe20500ed59884c
hhg <arcspatio@gmail.com>
2023-03-03 15:32:00 -0500

    draw lhs with automatic read in par_lim

--- emu_Nx2pt/gen_train_set/draw_pco_params.py
+++ emu_Nx2pt/gen_train_set/draw_pco_params.py
@@ -3,6 +3,7 @@ from pyDOE import lhs
 import time
 import argparse
 import pickle
+import pandas as pd
 
 def rescale(arr_in, domain):
     return domain[0] + arr_in * (domain[1]-domain[0])
@@ -31,6 +32,17 @@ def draw_lhs_samples(Npts, par_lim, criterion='c'):
 
     return samples_dict
 
+def gen_par_lim(file_chainsummary):
+    '''Generate the par_lim dictionary for each of the parameters with their boundary range [low, high], given the chain summary file
+    '''
+    df = pd.read_csv(file_chainsummary, index_col=0)
+
+    par_lim = {}
+    for par in df.index:
+        par_lim[par] = [df.loc[par].low, df.loc[par].high]
+    
+    return par_lim
+
 
 def main(args=None):
     '''
@@ -38,7 +50,7 @@ def main(args=None):
             run a test (on genie)
             >> cd /home/hhg/Research/emu_Nx2pt/repo/emulator_Nx2pt/
             >> python3 emu_Nx2pt/gen_train_set/draw_pco_params.py 100
-            >> python3 emu_Nx2pt/gen_train_set/draw_pco_params.py 200000
+            >> python3 emu_Nx2pt/gen_train_set/draw_pco_params.py 1000000
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument('Npts', help='Number of points to draw on lhs')
@@ -47,13 +59,15 @@ def main(args=None):
     Npts = int(args.Npts)
     print(f'--- Drawing {Npts} cosmology parameters ---')
 
-    par_lim = {
-        'Omega_m': [0.2, 0.4],
-        'sigma_8': [0.8, 0.85],
-        'Omega_b': [0.042, 0.050],
-        'n_s': [0.9, 1.02],
-        'h': [0.6, 0.8],
-    }
+    # generate par_lim from the file (One can also key in the range of parameters)
+    par_lim = gen_par_lim('/home/hhg/Research/emu_Nx2pt/data/RomanxSO_10x2pt_modified_w0wa_1Dsummary.csv')
+    # par_lim = {
+    #     'Omega_m': [0.2, 0.4],
+    #     'sigma_8': [0.8, 0.85],
+    #     'Omega_b': [0.042, 0.050],
+    #     'n_s': [0.9, 1.02],
+    #     'h': [0.6, 0.8],
+    # }
 
     pco_samples = draw_lhs_samples(Npts=Npts, par_lim=par_lim)
 
