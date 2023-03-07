db5d3c2b0bcf257089025c88a7301f17e44ce2ae
hhg <arcspatio@gmail.com>
2023-03-06 19:33:50 -0500

    training scripts

---
+++ scripts/train_MLP_emulator.py
@@ -0,0 +1,31 @@
+import os
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path+'/..')
+
+from emu_Nx2pt.utils import get_config_from_yaml
+from emu_Nx2pt.mlp_emulator import MLP_Emulator
+
+def main(args=None):
+    '''
+        Usage:
+            run a test (on yoda)
+            >> cd /home/hhg/Research/emu_Nx2pt/repo/emulator_Nx2pt/
+            >> python3 scripts/train_MLP_emulator.py --config configs/mlp_test.yaml > experiments/logs/mlp_test.log
+    '''
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', required=True,
+                        help='config file for training settings')
+
+    opt = parser.parse_args()
+    print(opt)
+
+    config = get_config_from_yaml(opt.config)
+    emu = MLP_Emulator(config=config)
+    emu.train()
+    #avg_test_chi2 = emu.test(input_dataloader)
+
+if __name__ == '__main__':
+    main()
