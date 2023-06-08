2492e564150ef314a413963fab8c25be0af07804
Hung-Jin Huang <hungjinh@users.noreply.github.com>
2023-06-08 17:35:45 -0400

    Develop attention-based network (#7)
    * [Major] Change the code structure in models to adapt to adding more structures.
    
    * Finish implemented AttentionBasedMLP
    
    * making sure the training pipeline is compatible with the AttentionBasedMLP
    
    * example config for running AttentionBasedMLP
    
    * update code to ensure compatibility with PyTorch 2.0
    
    * [debug] add Wo to the output of MultiHeadAttention block
    
    * vectorize MultiHeadAttention block
    
    * [minor] renaming files
    
    * [minor] add computation time statistics in readme

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Building Nx2pt emulators to accelerate cosmological inference pipeline. 
 
-[Simple MLP Network Training Logs](./docs/training_logs_MLP.md)
+- Typical computation time for a 10x2 point data vector in CosmoLike takes ~ 2.6 s.
+
+- The computation time for a 10x2 point data vector from neural network based emulator ~ 1.1 ms, which is O(10^3) improvement.
--- emu_Nx2pt/models/blocks.py
+++ emu_Nx2pt/models/blocks.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -52,46 +53,97 @@ class Res_Block(nn.Module):
 
         return F.relu(identity + residual * self.scale_factor)
 
-class MultiHeadAttention(nn.Module):
 
-    def __init__(self, d_embed, n_heads):
+class DotProductAttention(nn.Module):
+    '''
+    Args:
+        p_dropout: probability of an element to be zeroed. Default: 0., i.e. no dropout
+    '''
+
+    def __init__(self, dropout_p=0.):
         super().__init__()
-        self.d_embed = d_embed # embedded dimension for each token in a sequence
-        self.n_heads = n_heads
+        self.dropout = nn.Dropout(dropout_p)
 
-        assert d_embed % n_heads == 0, f"Can't divide dimension {d_embed} into {n_heads} heads"
+    def forward(self, queries, keys, values):
+        d = queries.shape[-1]
 
-        d_head = int(d_embed / n_heads)
+        scores = (queries @ keys.transpose(-2, -1))/math.sqrt(d)
+        self.attn_weights = nn.functional.softmax(scores, dim=-1)
 
-        self.Wq = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-        self.Wk = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-        self.Wv = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-        self.d_head = d_head
-        self.softmax = nn.Softmax(dim=-1)
+        return self.dropout(self.attn_weights) @ values
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads, dropout_p=0., bias=True):
+        super().__init__()
+        self.num_heads = num_heads
+        self.attention = DotProductAttention(dropout_p)
+
+        self.Wq = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.Wk = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.Wv = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.Wo = nn.Linear(embed_dim, embed_dim, bias=bias)
+
+    def forward(self, queries, keys, values, need_weights=False, average_attn_weights=False):
+        queries = self.transpose_qkv(self.Wq(queries))
+        keys = self.transpose_qkv(self.Wq(keys))
+        values = self.transpose_qkv(self.Wq(values))
+
+        output = self.attention(queries, keys, values) # (batch_size*num_heads, n_seq, embed_dim/num_heads)
+        output_concat = self.transpose_output(output)  # (batch_size, n_seq, embed_dim)
+
+        if need_weights:
+            ori_attn_weights = self.attention.attn_weights
+            ori_attn_weights = ori_attn_weights.reshape(-1, self.num_heads, ori_attn_weights.shape[1], ori_attn_weights.shape[2])
+            if average_attn_weights:
+                return self.Wo(output_concat), ori_attn_weights.mean(dim=1)
+            else:
+                return self.Wo(output_concat), ori_attn_weights
+        else:
+            return self.Wo(output_concat)
+
+    def transpose_qkv(self, X):
+        '''Transposition for parallel computation of multiple attention heads.
+            input  X.shape = (batch_size, n_seq, embed_dim) 
+            output X.shape = (batch_size*num_heads, n_seq, embed_dim/num_heads)
+        '''
+        X = X.reshape(X.shape[0], X.shape[1], self.num_heads, -1)
+        X = X.permute(0, 2, 1, 3)
+        return X.reshape(-1, X.shape[2], X.shape[3])
+
+    def transpose_output(self, X):
+        '''Reverse the operation of transpose_qkv.'''
+        X = X.reshape(-1, self.num_heads, X.shape[1], X.shape[2])
+        X = X.permute(0, 2, 1, 3)
+        return X.reshape(X.shape[0], X.shape[1], -1)
+
+
+class AddNorm(nn.Module):
+    def __init__(self, norm_shape, dropout_p=0.):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout_p)
+        self.ln = nn.LayerNorm(norm_shape)
     
-    def forward(self, sequences):
-        # Sequences has shape (N, seq_length, d_embed), where d_embed = token dimension
-        result = []
-        for sequence in sequences:
-            seq_result = []
-            for head in range(self.n_heads):
-                Wq = self.q_mappings[head]
-                Wk = self.k_mappings[head]
-                Wv = self.k_mappings[head]
-
-                seq = sequence[:, head * self.d_head: (head + 1) * self.d_head]
-                q, k, v = Wq(seq), Wk(seq), Wv(seq)
-
-                attention = self.softmax(q @ k.T / (self.d_head ** 0.5))
-                seq_result.append(attention @ v)
-            result.append(torch.hstack(seq_result))
-        return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
+    def forward(self, X, Y):
+        return self.ln( self.dropout(Y) + X )
 
 
-class Reshape(nn.Module):
-    def __init__(self, shape):
-        super(Reshape, self).__init__()
-        self.shape = shape
+class TransformerEncoderBlock(nn.Module):
+    def __init__(self, embed_dim, num_heads, mlp_ratio=1, dropout_p=0., bias=True):
+        super().__init__()
 
-    def forward(self, x):
-        return x.view(x.size(0), *self.shape)
+        self.mha = MultiHeadAttention(embed_dim, num_heads, dropout_p, bias)
+
+        self.addnorm1 = AddNorm(embed_dim, dropout_p)
+
+        self.ffn = nn.Sequential(
+            nn.Linear(embed_dim, mlp_ratio * embed_dim),
+            nn.GELU(),
+            nn.Linear(mlp_ratio * embed_dim, embed_dim)
+        )
+
+        self.addnorm2 = AddNorm(embed_dim, dropout_p)
+
+    def forward(self, X):
+        Y = self.addnorm1(X, self.mha(X, X, X))
+        return self.addnorm2(Y, self.ffn(Y))
